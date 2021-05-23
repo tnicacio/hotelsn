55d10a181cd581a969fa51395ee6f098c22a6686
tnicacio <nicacio.t92@gmail.com>
2021-05-23 18:36:24 -0300

    Garage Controller - all implementations succeeded

--- src/main/java/com/tnicacio/seniorhotel/controllers/GarageController.java
+++ src/main/java/com/tnicacio/seniorhotel/controllers/GarageController.java
@@ -5,8 +5,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,5 +37,23 @@ public class GarageController {
 	public ResponseEntity<List<GarageDTO>> findAll() {
 		List<GarageDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<GarageDTO> findById(@PathVariable(value = "id") Long id) {
+		GarageDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<GarageDTO> update(@PathVariable(value = "id") Long id, @RequestBody GarageDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable(value = "id") Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
